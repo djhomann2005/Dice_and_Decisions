adfcf60c4e9d96d9e12fc150f3c44bc1b230da97
djhomann2005 <djhomann2005@gmail.com>
2026-02-10 20:42:44 -0600

    Added package.json; Added & test dice roll and message selection

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
-// Function to run when button is clicked
+// Function to get random d20 roll
 function rollDice() {
     const roll = Math.floor(Math.random() * 20 + 1);
     return roll;
 }
 
+// Determine Oracle message based on roll
+function getMessage(roll) {
+    let message = '';
+
+    if ( roll <= 7 ) {
+        message = "The Oracle says No";
+    } else if ( roll <= 13 ) {
+        message = "The Oracle says maybe. Either it is a partial result, or reroll if a yes/no answer is needed";
+    } else {
+        message = "The Oracle says Yes";
+    }
+
+    return message;
+}
+
+// display result on webpage
 function displayResult() {
     
 }
 
-// Wait for the DOM to load before adding the event listener
-document.addEventListener('DOMContentLoaded', (event) => {
-    const button = document.getElementById('myButton');
-    if (button) {
-        button.addEventListener('click', rollDice);
-    }
-});
+// code for testing
+let result = rollDice();
+console.log(getMessage(result));
